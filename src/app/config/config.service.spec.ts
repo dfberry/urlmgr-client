884e79b0fb706e2f31e5effc1d610f4a30a0f897
dfberry <dinaberry@outlook.com>
2017-05-01 13:47:45 -0700

    http error in config service mocked so utest now passes

--- src/app/config/config.service.spec.ts
+++ src/app/config/config.service.spec.ts
@@ -1,6 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
 import { ConfigService } from './config.service';
-import { Http } from '@angular/http';
+
+
+import { MockBackend } from '@angular/http/testing';
+import { HttpModule, Http, BaseRequestOptions, XHRBackend, ResponseOptions } from '@angular/http';
+
+// TBD: import actual config settings from ./config.json
 
 describe('Service: ConfigService', () => {
   let service: ConfigService;
@@ -11,11 +16,40 @@ describe('Service: ConfigService', () => {
     };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      providers: [ ConfigService ]
+      imports: [HttpModule],
+      providers: [ 
+        ConfigService,
+        BaseRequestOptions,
+        MockBackend,
+        { provide: XHRBackend, useClass: MockBackend }
+      ]
     });
     service = TestBed.get(ConfigService);
   }));
   it(`should create instance of service`, async(() => {
     expect(service).toBeDefined();
+  }));
+  it(`should load config file successfully`, async(() => {
+    inject([ConfigService, XHRBackend], (configService, mockBackend) => {
+
+      const mockResponse = {
+        "livetest": "wednesday 11:02am",
+        "clientHost": "localhost:3000",
+        "apiUrl": "http://urlmgrapi.dfberry.io/v1/",
+        "xxx": "http://localhost:3003/v1/",
+        "rssProxy": "http://api.rss2json.com/v1/api.json?rss_url=",
+        "title": "Url Manager"
+      };
+
+      mockBackend.connections.subscribe((connection) => {
+
+        connection.mockRespond(new Response(new ResponseOptions({
+          body: JSON.stringify(mockResponse)
+        })));
+
+        configService.load();
+        expect(JSON.parse(JSON.stringify(configService.config))).toBe(JSON.parse(JSON.stringify(mockResponse)));
+      });
+    });
   }));
 });
