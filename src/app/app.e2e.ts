a1256cc84dbdb0821730704c4567c31ed9a5ccb1
dfberry <dinaberry@outlook.com>
2017-05-01 14:02:18 -0700

    basic e2e testing begun

--- src/app/app.e2e.ts
+++ src/app/app.e2e.ts
@@ -8,26 +8,34 @@ describe('App', () => {
 
   it('should have a title', () => {
     let subject = browser.getTitle();
-    let result  = 'Angular2 Webpack Starter by @gdi2290 from @AngularClass';
-    expect(subject).toEqual(result);
-  });
-
-  it('should have header', () => {
-    let subject = element(by.css('h1')).isPresent();
-    let result  = true;
-    expect(subject).toEqual(result);
-  });
-
-  it('should have <home>', () => {
-    let subject = element(by.css('app home')).isPresent();
-    let result  = true;
+    let result  = 'Url Manager';
     expect(subject).toEqual(result);
   });
 
   it('should have buttons', () => {
     let subject = element(by.css('button')).getText();
-    let result  = 'Submit Value';
+    let result  = 'Login';
     expect(subject).toEqual(result);
   });
 
+  describe('App Auth', () => {
+    //http://product.moveline.com/testing-angular-apps-end-to-end-with-protractor.html
+
+    let loginURL;
+    //let email = element(by.name('login-email'));
+    //let password = element(by.name('login-password'));
+    //let loginButton = element(by.xpath('//form[1]/input[@type="submit"]'));
+    //let error = element(by.model('loginError'));
+
+    it('should redirect to the login page if trying to load protected page while not authenticated', () => {
+      browser.get('/#/login');
+      loginURL = browser.getCurrentUrl();
+
+      browser.get('/#/dashboard');
+      expect(browser.getCurrentUrl()).toEqual(loginURL);
+    });
+    //it('should warn on missing/malformed credentials', fail);
+    //it('should accept a valid email address and password', fail);
+    //it('should return to the login page after logout', fail);
+  });
 });
