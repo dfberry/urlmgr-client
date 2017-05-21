0c272c459986874076fbc8fd6fda4fb5c6bcd3b5
dfberry <dinaberry@outlook.com>
2017-05-21 14:43:57 -0700

    removed unused code from url module's url service

--- src/app/url/url/url.service.ts
+++ src/app/url/url/url.service.ts
@@ -4,7 +4,6 @@ import { ActionReducer, Action, Store } from '@ngrx/store';
 import { Http, Response, URLSearchParams, Headers, RequestOptionsArgs} from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import { createSelector } from 'reselect';
-import * as cheerio from 'cheerio';
 
 import { type } from '../../utils/index';
 import { HttpDataService} from '../../services/index';
@@ -19,53 +18,40 @@ import { UrlEvent } from './url.event';
 @Injectable()
 export class UrlService  {
 
-    items:Observable <Url[]>;
     public count: Observable<number>;
     public next: Observable<number>;
+
+    // state's url list
+    items:Observable <Url[]>;
+
+    // TBD: why keep this around? set by methods
     user: User;
 
-    currentConfig;
+    // App Config's base url
     baseUrl;
 
     constructor(
         private store:Store<AppState>, 
         private _httpDataService: HttpDataService,
-        //private _FeedDefinitionService: FeedDefinitionService,
         private configService: ConfigService,
         private urlEvent: UrlEvent,
-        //private feedService: FeedService
     ){
-        //console.log("url.ts - ctor");
         this.items = store.select(state => state.urls);
         this.baseUrl = configService.get("apiUrl") + "urls/";
     }
 
-    getTokenedHeaders(){
-        // add user auth token to header
-        let headers = new Headers();
-        headers.set('x-access-token', this.user['token']);
-        let options:RequestOptionsArgs = {
-            headers : headers
-        };
-        return options;
-    }
-    getQueryStringWithUserId(){
-        let params: URLSearchParams = new URLSearchParams();
-        params.set("user", this.user['id']);
-        return params;
-    }
-    getBodyWithUserId(){
-        return {user: this.user['id']};
-    }
-
+    /*
+        get user's items from server
+        return Promise<Url[]>
+    */
     loadItems(user){
+        var self = this;
         return new Promise<Url[]>((resolve, reject) => {
 
             if(!user || !user.id) {
-                //console.log("urlService::loadItems - user is empty");
                 reject("user is empty");
             }
-            this.user = user;
+            self.user = user;
 
             let headers = new Headers();
             headers.set('x-access-token', user['token']);
@@ -74,25 +60,36 @@ export class UrlService  {
                 headers : headers
             };
 
-            return this._httpDataService.getJsonPromise(this.baseUrl + "?user=" + user["id"], options)
+            self._httpDataService.getJsonPromise(self.baseUrl + "?user=" + user["id"], options)
                 .then(data => {
                     resolve(data);
                 }).catch(err => {
-                        console.log(err);
-                        reject(err);
+                    console.log(err);
+                    reject(err);
                 });
 
         });
     }
+    /*
+        get properties from server
+        return promise<json>
+
+        get url title
+        get url's feeds
+    */
     getUrlProperties(url, user){
         var self = this;
         return new Promise<Object>((resolve, reject) => {
+
+            if(!user || !user.id) reject("invalid user");
+
+            if(!url) reject("invalid url");
 
             let post = {
                 url: url
             };
 
-            this._httpDataService.postWithAuthReturnText(this.baseUrl + "meta/", post, user)
+            self._httpDataService.postWithAuthReturnText(self.baseUrl + "meta/", post, user)
             .then(meta => {
                 let metaObj = JSON.parse(meta);
 
@@ -101,7 +98,7 @@ export class UrlService  {
                 if(metaObj && metaObj.data && metaObj.data.feeds && metaObj.data.feeds)
                 {
                     for(var i = 0;i<metaObj.data.feeds.length; i ++){
-                    feed.push(metaObj.data.feeds[i].href);
+                        feed.push(metaObj.data.feeds[i].href);
                     }
                 }
                 if(metaObj && metaObj.data && metaObj.data.title)
@@ -115,146 +112,94 @@ export class UrlService  {
             });
         });
     }
-    getTitle(html){
-        let $ = cheerio.load(html);
-        let title = $("title").text();
-        return title;
-    }
-    getLinks(html){
-        let list = [];
-        let $ = cheerio.load(html);
-        $('link').each( function () {
-            let type = $(this).attr("type");
-            let href = $(this).attr("href");
-            if (type){
-                type = type.replace('\\"','');
-                type = type.replace('\\"','');
+    /*
+        enter item in server-side db than trigger reload
+        return Promise<json>
+    */
+    insertItem(user: User, item: any){
+        var self = this;
+        return new Promise<Object>((resolve, reject) => {
 
-                if (href){
-                    href = href.replace('\\"','');
-                    href = href.replace('\\"','');
-                }
-                list.push({type: type, href: href});
-            }
-            
-        });
-        return list.sort();
-    }
-    getUrlHtml(url, user){
-        return new Promise<string>((resolve, reject) => {
-            // post form
-            let post = {
-                url: url,
-                user: user.id
-            }
+            if(!user || !user.id) reject("invalid user");
+            self.user = user;
 
-            // headers
+            if(!item || !item.user) reject("invalid item");
+            item["user"] = self.user.id
+
             let headers = new Headers();
             headers.set('x-access-token', user['token']);
             headers.set('Content-Type', 'application/json');
 
-            let options: RequestOptionsArgs = {
+            let options:RequestOptionsArgs = {
                 headers : headers
             };
 
-            return this._httpDataService.postHtmlData(this.baseUrl + "feeds/", post, options)
-                .then(data => {
-                    resolve(data || "");
-                }).catch(err => {
+            self._httpDataService.postJsonData(self.baseUrl, item, options).then((data) => {   
+
+                    // TODO: what should happen if there is an error on the server/api side
+                    // how would I should the error? 
+
+                    // reload
+                    self.urlEvent.fire('URL_ADD_1'); 
+
+                    // TBD: is this the server-side mongo doc for the item? 
+                    resolve(data);  
+                })
+                .catch((err) => {
                     console.log(err);
                     reject(err);
                 });
         });
     }
 
-    insertItem(user: User, item: any){
-
-        if(!user || !user.id) return;
-        this.user = user;
-
-        item["user"] = this.user.id
-
-        let headers = new Headers();
-        headers.set('x-access-token', user['token']);
-        headers.set('Content-Type', 'application/json');
-
-        let options:RequestOptionsArgs = {
-            headers : headers
-        };
-
-        //console.log("url insert item = " + JSON.stringify(item));
-        //console.log("url insert options = " + JSON.stringify(options))
-
-        return this._httpDataService.postJsonData(this.baseUrl, item, options).then((data) => {
-                
-                //this._FeedDefinitionService.getFeedUrl(item);
-
-                // TODO: what should happen if there is an error on the server/api side
-                // how would I should the error? 
-                //console.log("url.ts::insertItem - returned data = " + JSON.stringify(data));
-                this.urlEvent.fire('URL_ADD_1');  
-                return data;  
-            })
-            .catch((err) => {
-                console.log(err);
-                return err;
-            });
-    }
-
-    // tell store about change
-    private insertToStore(newItem){
-        //this.store.dispatch({ type: ADD_URL, payload: newItem});
-    }
-
-    // delete item
-
+    /*
+        delete item in server-side db than trigger reload
+        return Promise<json>
+    */
     removeItem(user, item:Url){
-        //console.log("item deleted = " + item.url);
+        var self = this;
+        return new Promise<Object>((resolve, reject) => {
 
-        if(!user || !user.id) {
-            //console.log("UrlService::removeItem, user is empty");
-            return;
-        }
-        this.user = user;
+            if(!user || !user.id) {
+                reject("user is invalid");
+            }
+            self.user = user;
 
-        let urlId = item["id"] ? item["id"] : item["_id"];
-        if (!urlId) {
-            //console.log("UrlService::removeItem, urlId is empty");
-            return;
-        }
+            let urlId = item["id"] ? item["id"] : item["_id"];
+            if (!urlId) {
+                reject("url is invalid");
+            }
 
-        let postForm = {
-            user: this.user['id'],
-        };
+            let postForm = {
+                user: self.user['id'],
+            };
 
-        let headers = new Headers();
-        headers.set('x-access-token', this.user['token']);
+            let headers = new Headers();
+            headers.set('x-access-token', self.user['token']);
 
-        let options:RequestOptionsArgs = {
-            headers : headers,
-            body : postForm
-        };
+            let options:RequestOptionsArgs = {
+                headers : headers,
+                body : postForm
+            };
 
-        //console.log("UrlService::removeItem, options = " + JSON.stringify(options));
+            self._httpDataService.delete(self.baseUrl + urlId, options)
 
-        return this._httpDataService.delete(this.baseUrl + urlId, options)
+                .then((data) => {
 
-            .then((data) => {
-                //console.log("url.ts removeItem http service delete success");
-                // TODO: what should happen if there is an error on the server/api side
-                // how would I should the error? 
-                //console.log("url.ts::remoteItem - returned data = " + JSON.stringify(data));
-                //this.loadItems(this.user); 
+                    // TODO: what should happen if there is an error on the server/api side
+                    // how would I should the error? 
 
-                // really just need it to reload
-                this.urlEvent.fire('URL_ADD_1'); 
-                return data;  
-            
-            })
-            .catch((err) => {
-                console.log(err);
-                return err;
-            });
+                    // reload
+                    self.urlEvent.fire('URL_ADD_1'); 
 
+                    // TBD: what is data? 
+                    resolve(data);  
+                
+                })
+                .catch((err) => {
+                    console.log(err);
+                    reject(err);
+                });
+        });
     }
 }
